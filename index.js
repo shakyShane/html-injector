9d7c0ece0e25851de69c955eaa13bf6ba6faced3
Shane Osbourne <shane.osbourne8@gmail.com>
2014-08-10 17:47:41 +0100

    Added specs for dom-comparisons

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var request     = require('request');
 
 var PLUGIN_NAME = "HTML Injector";
 
+var defaults = {
+    excludedTags: ["HTML", "HEAD", "BODY"]
+};
+
 module.exports = {
 
     /**
@@ -37,99 +41,186 @@ module.exports = {
                 url = data.url;
                 request(url, function (error, response, body) {
                     if (!error && response.statusCode == 200) {
-                        oldDom = jsdom(body);
+                        oldDom = createDom(body);
                     }
                 });
             });
         });
 
         bs.events.on("file:changed", function (data) {
-
-            if (!url) {
+            if (!url || !oldDom || data.namespace !== PLUGIN_NAME) {
                 return;
             }
-
-            if (data.namespace !== PLUGIN_NAME) {
-                return;
-            }
-
-            if (!oldDom) {
-                return;
-            }
-
-            request(url, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var newDom = jsdom(body);
-                    inject(oldDom, newDom);
-                    oldDom = newDom;
-                }
-            });
+            doNewRequest();
         });
+
+        bs.events.on("plugin:html:inject", function () {
+            if (!url || !oldDom) {
+                return;
+            }
+            doNewRequest();
+        });
+
+        function doNewRequest() {
+            requestNew(url, oldDom, function (window, diffs, newDom) {
+                inject(window, diffs);
+                oldDom = newDom;
+            }, opts);
+        }
+
     }
 };
 
 /**
- * @param {Socket.io} sockets
- * @param {Object} opts - plugin options
- * @returns {Function}
+ * @param diffs
+ * @param excludeList
+ * @returns {*}
  */
-function getInjector(sockets, opts) {
+function removeExcluded(diffs, excludeList) {
+    return diffs.filter(function (item) {
+        return !_.contains(excludeList, item.tagName);
+    });
+}
 
-    var count = 0;
-    var excludeList = opts.excludedTags || ["HTML", "HEAD", "BODY"];
+module.exports.removeExcluded = removeExcluded;
 
-    return function (oldDom, newDom) {
+/**
+ * Request new version of Dom
+ * @param {String} url
+ * @param {Object} oldDom
+ * @param {Object} opts - plugin options
+ * @param {Function} cb
+ */
+function requestNew (url, oldDom, cb, opts) {
 
-        var window      = newDom.parentWindow;
-        var compare = require('dom-compare').compare;
+    request(url, function (error, response, body) {
 
-        var result = compare(oldDom, newDom, {formatFailure: function (failure, node) {
+        if (!error && response.statusCode == 200) {
+
+            var newDom = createDom(body);
+            var diffs  = compareDoms(oldDom, newDom);
+            diffs      = removeDupes(diffs);
+//            diffs      = removeChildren(diffs); // unreliable right now
+            diffs      = removeExcluded(diffs, opts.excludedTags || defaults.excludedTags);
+
+            if (diffs) {
+                cb(newDom.parentWindow, diffs, newDom);
+            }
+
+            oldDom = newDom;
+        }
+    });
+}
+
+/**
+ * @param {Array} differences
+ * @returns {Array}
+ */
+function removeDupes(differences) {
+
+    return _.uniq(differences, "node");
+}
+
+module.exports.removeDupes = removeDupes;
+
+/**
+ * Because we replace the HTML of elements, differences of children are irrelevant
+ * so we don't bother with them.
+ * @param {Array} differences
+ * @returns {Array}
+ */
+function removeChildren(differences) {
+
+    differences.reverse();
+
+    var parents = [];
+
+    differences.forEach(function (item, index) {
+
+        var path = item.node;
+
+        if (index === 0) {
+            return parents.push(item);
+        }
+
+        parents.forEach(function (parentItem) {
+            if (!_.contains(path, parentItem.node)) {
+                return parents.push(item);
+            }
+        });
+    });
+
+    return parents;
+}
+
+module.exports.stripDupes = removeDupes;
+
+
+/**
+ * Compare two DOMS
+ * @param {Object} newDom
+ * @param {Object} oldDom
+ * @returns {*}
+ */
+function compareDoms(oldDom, newDom) {
+
+    var window      = newDom.parentWindow;
+    var compare = require('./node_modules/dom-compare').compare;
+
+    var result = compare(oldDom, newDom, {
+        formatFailure: function (failure, node) {
             var allElems    = node.ownerDocument.getElementsByTagName(node.nodeName);
             failure.index   = Array.prototype.indexOf.call(allElems, node);
             failure.tagName = node.nodeName;
             return failure;
-        }});
-
-        var same = result.getResult(); // false cause' trees are different
-
-        if (!same) {
-
-            var cldiff = result.getDifferences(); // array of diff-objects
-
-            var added = [];
-
-            cldiff.forEach(function (item) {
-
-                var alreadyAdded = added.some(function (addedItem) {
-                    return addedItem.tagName === item.tagName && addedItem.index === item.index;
-                });
-
-                if (!_.contains(excludeList, item.tagName) && !alreadyAdded) {
-
-                    count += 1;
-
-                    added.push(item);
-
-                    var element = window.document.getElementsByTagName(item.tagName)[item.index];
-
-                    var elemAttrs = {};
-
-                    for (var attr, i=0, attrs=element.attributes, l=attrs.length; i<l; i++){
-                        attr = attrs.item(i)
-                        elemAttrs[attr.nodeName] = attr.nodeValue
-                    }
-
-                    if (element) {
-                        sockets.emit("html:inject", {
-                            html: element.innerHTML,
-                            tagName: item.tagName,
-                            index: item.index,
-                            cssText: element.style.cssText,
-                            attrs: elemAttrs
-                        });
-                    }
-                }
-            });
         }
+    });
+
+    var same = result.getResult(); // false cause' trees are different
+
+    if (!same) {
+        return result.getDifferences(); // array of diff-objects
+    }
+}
+
+module.exports.compareDoms = compareDoms;
+
+/**
+ * @param string
+ * @returns {*}
+ */
+function createDom(string) {
+    return jsdom(string);
+}
+
+/**
+ * @param {Socket.io} sockets
+ * @returns {Function}
+ */
+function getInjector(sockets) {
+
+    return function (window, diffs) {
+
+        diffs.forEach(function (item) {
+
+            var element = window.document.getElementsByTagName(item.tagName)[item.index];
+
+            var elemAttrs = {};
+
+            for (var attr, i=0, attrs=element.attributes, l=attrs.length; i<l; i++){
+                attr = attrs.item(i);
+                elemAttrs[attr.nodeName] = attr.nodeValue
+            }
+
+            if (element) {
+                sockets.emit("html:inject", {
+                    html: element.innerHTML,
+                    tagName: item.tagName,
+                    index: item.index,
+                    cssText: element.style.cssText,
+                    attrs: elemAttrs
+                });
+            }
+        });
     }
 }
---
+++ test/compare.doms.js
@@ -0,0 +1,226 @@
+var compareDoms   = require("../index").compareDoms;
+var stripDupes    = require("../index").removeDupes;
+var stripExcluded = require("../index").removeExcluded;
+var jsdom       = require("jsdom").jsdom;
+var fs          = require("fs");
+var assert      = require("chai").assert;
+var _           = require("lodash");
+var multiline   = require("multiline");
+
+
+//var complex1         = fs.readFileSync(__dirname + "/fixtures/store-product.html", "utf-8");
+//var complex1Modified = fs.readFileSync(__dirname + "/fixtures/store-product-alt.html", "utf-8");
+//var complex2         = fs.readFileSync(__dirname + "/fixtures/web-starter-kit-styleguide.html", "utf-8");
+//var complex2Modified = fs.readFileSync(__dirname + "/fixtures/web-starter-kit-styleguide-modified.html", "utf-8");
+
+describe("Comparing Simple doms", function(){
+
+    it("returns element index when it has different children", function(){
+
+        var str1 = multiline.stripIndent(function(){/*
+         <!doctype html>
+         <html>
+             <body>
+                <h1>❤ unicorns</h1>
+             </body>
+         </html>
+         */});
+        var str2 = multiline.stripIndent(function(){/*
+         <!doctype html>
+         <html>
+             <body>
+                <h1><span>❤</span> unicorns</h1>
+             </body>
+         </html>
+         */});
+
+        var oldDom = jsdom(str1);
+        var newDom = jsdom(str2);
+
+        var diffs = stripDupes(compareDoms(oldDom, newDom));
+
+        assert.equal(diffs.length, 1);
+        assert.equal(diffs[0].tagName, "H1");
+        assert.equal(diffs[0].index, "0");
+    });
+
+
+    it("returns element index when it has missing children", function(){
+
+        var str1 = multiline.stripIndent(function(){/*
+         <!doctype html>
+         <html>
+             <body>
+             </body>
+         </html>
+         */});
+        var str2 = multiline.stripIndent(function(){/*
+         <!doctype html>
+         <html>
+             <body>
+                <h1><span>❤</span> unicorns</h1>
+             </body>
+         </html>
+         */});
+
+        var oldDom = jsdom(str1);
+        var newDom = jsdom(str2);
+
+        var diffs = stripDupes(compareDoms(oldDom, newDom));
+
+        assert.equal(diffs.length, 1);
+        assert.equal(diffs[0].tagName, "BODY");
+        assert.equal(diffs[0].index, "0");
+    });
+
+    it("Removes duplicate diffs if on same element", function(){
+
+        var str1 = multiline.stripIndent(function(){/*
+         <!doctype html>
+         <html>
+             <body>
+                <h1><span>❤s</span> unicorns <i>What you saying?</i></h1>
+             </body>
+         </html>
+         */});
+        var str2 = multiline.stripIndent(function(){/*
+         <!doctype html>
+         <html>
+             <body>
+                <h1><span>❤</span> unicorns</h1>
+             </body>
+         </html>
+         */});
+
+        var oldDom = jsdom(str1);
+        var newDom = jsdom(str2);
+
+        var diffs = stripDupes(compareDoms(oldDom, newDom));
+
+        assert.equal(diffs.length, 2);
+        assert.equal(diffs[0].tagName, "SPAN");
+    });
+
+    it("Removes duplicate diffs if on same element", function(){
+
+        var str1 = multiline.stripIndent(function(){/*
+            <!doctype html>
+            <html lang="en-US">
+            <head>
+                <meta charset="UTF-8">
+                <title>This has a Title</title>
+            </head>
+            <body>
+                <h1>HTML injector</h1>
+                <h1 class="hidden" style="background: red;">HTML injector is prettty cool</h1>
+            </body>
+            </html>
+         */});
+        var str2 = multiline.stripIndent(function(){/*
+            <!doctype html>
+            <html lang="en-US">
+            <head>
+                <meta charset="UTF-8">
+                <title>This has a Title</title>
+                <style>
+                    .hidden {}
+                </style>
+                <link href="style.css"></link>
+            </head>
+            <body>
+                <h1>HTML injector</h1>
+                <h1 class="hidden" style="background: red;">HTML injector is prettty cool</h1>
+            </body>
+            </html>
+         */});
+
+        var oldDom = jsdom(str1);
+        var newDom = jsdom(str2);
+
+        var diffs = stripDupes(compareDoms(oldDom, newDom));
+
+        assert.equal(diffs.length, 1);
+        assert.equal(diffs[0].tagName, "HEAD");
+        assert.equal(diffs[0].index, "0");
+    });
+
+
+    it("Returns diffs from mulitple elements", function(){
+
+        var str1 = multiline.stripIndent(function(){/*
+            <!doctype html>
+            <html lang="en-US">
+            <head>
+                <meta charset="UTF-8">
+                <title>This has a Title</title>
+            </head>
+            <body>
+                <h1>HTML injector</h1>
+                <h1 class="hidden" style="background: red;">HTML injector is prettty cool</h1>
+            </body>
+            </html>
+         */});
+        var str2 = multiline.stripIndent(function(){/*
+            <!doctype html>
+            <html lang="en-US">
+            <head>
+                <meta charset="UTF-8">
+                <title>This has a Title</title>
+                <style>
+                    .hidden {}
+                </style>
+                <link href="style.css"></link>
+            </head>
+            <body>
+                <h1>HTML injector</h1>
+                <h1 class="hidden" style="background: red;">HTML injector is prettty coolsss</h1>
+            </body>
+            </html>
+         */});
+
+        var oldDom = jsdom(str1);
+        var newDom = jsdom(str2);
+
+        var diffs = stripDupes(compareDoms(oldDom, newDom));
+
+        assert.equal(diffs.length, 2);
+        assert.equal(diffs[0].tagName, "HEAD");
+        assert.equal(diffs[1].tagName, "H1");
+    });
+
+});
+
+describe("Removing excluded", function(){
+
+    it("returns a filtereed list", function(){
+
+        var str1 = multiline.stripIndent(function(){/*
+         <!doctype html>
+         <html>
+         <body>
+         <h1>❤ unicornsss</h1>
+         <h2><span>HATE</span> unicornsss</h2>
+         <h3>Hi there</h3>
+         </body>
+         </html>
+         */});
+        var str2 = multiline.stripIndent(function(){/*
+         <!doctype html>
+         <html>
+         <body>
+         <h1>❤ unicornsss</h1>
+         <h2><span>HATE</span> unicornsssas</h2>
+         <h3 class="hidden">Hi there</h3>
+         </body>
+         </html>
+         */});
+
+        var oldDom = jsdom(str1);
+        var newDom = jsdom(str2);
+
+        var diffs = stripExcluded(stripDupes(compareDoms(oldDom, newDom)), ["H1", "H3"]);
+
+        assert.equal(diffs.length, 1);
+        assert.equal(diffs[0].tagName, "H2");
+    });
+});
